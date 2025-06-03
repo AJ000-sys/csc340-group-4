5fc35459da86fbf47f5b0591fad49c4127063821
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-03 12:22:08 -0400

    Created backend-api folder

---
+++ backend-api/src/main/java/com/example/Simplex/SimplexApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplexApplication.class, args);
+	}
+
+}
