85123c4cd6bf285dcce61652004917b8ac80f114
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-04 14:57:36 -0400

    Updated

--- backend-api/src/main/java/com/example/Simplex/User/UserController.java
+++ backend-api/src/main/java/com/example/Simplex/User/UserController.java
@@ -9,7 +9,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserController {
-    
-    
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public Object getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{userId}")
+    public Object getUserById(@PathVariable long userId) {
+        return userService.getUsersById(userId);
+    }
+
+    @GetMapping("/users/name/{userName}")
+    public Object getUserByName(@PathVariable String userName) {
+        return userService.getUserByName(userName);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/users/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody User user) {
+        return userService.updateUser(userId, user);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
 }
--- backend-api/src/main/java/com/example/Simplex/User/UserRepository.java
+++ backend-api/src/main/java/com/example/Simplex/User/UserRepository.java
@@ -1,5 +1,14 @@
 package com.example.Simplex.User;
 
-public class UserRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "Select * from users where user_name = ?1", nativeQuery = true)  
+    List<User> findByUserName(String userName);
 }
