203358ff6795cb52037d968ee1453c7ceed1430c
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-04 11:13:33 -0400

    updated

--- backend-api/src/main/java/com/example/Simplex/User.java
+++ backend-api/src/main/java/com/example/Simplex/User.java
@@ -1,5 +1,61 @@
 package com.example.Simplex;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
-    
+
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+
+    public User(){
+
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(Long id, String userName, String password, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
