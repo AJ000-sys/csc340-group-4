57c53778e2885e1567950b404b18dbb9d14a0e25
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-04 14:14:39 -0400

    updated UserController

--- backend-api/src/main/java/com/example/Simplex/User/UserService.java
+++ backend-api/src/main/java/com/example/Simplex/User/UserService.java
@@ -1,5 +1,34 @@
 package com.example.Simplex.User;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
 public class UserService {
     
+    private UserRepository userRepository;
+
+    public Object getAllUsers() {
+        return userRepository.findAll();
+    }   
+
+    public Object getUsersById(@PathVariable long userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    public Object getUserByName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long userId, User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
