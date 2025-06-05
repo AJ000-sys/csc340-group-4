2c2ca706c138517ded4e9bdb3473aa3518e3be0f
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-04 20:54:27 -0400

    Added new files

---
+++ backend-api/src/main/java/com/example/Simplex/Review/ReplyService.java
@@ -0,0 +1,5 @@
+package com.example.Simplex.Review;
+
+public class ReplyService {
+    
+}
---
+++ backend-api/src/main/java/com/example/Simplex/Review/Review.java
@@ -0,0 +1,18 @@
+package com.example.Simplex.Review;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long reviewId;
+    
+}
