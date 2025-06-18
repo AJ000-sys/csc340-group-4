523902ca78db26d1800a251e690e9f9a17babb4d
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-18 08:50:21 -0400

    Update

--- backend-api/src/main/java/com/example/Simplex/Song/Song.java
+++ backend-api/src/main/java/com/example/Simplex/Song/Song.java
@@ -8,6 +8,7 @@ import com.example.Simplex.Comment;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
